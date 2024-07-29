4e48ed810c9b88287268c69c009086fce25f5d55
mohammad <m.deheghanifard@gmail.com>
2024-07-29 09:53:14 +0330

    add colors to project

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+
+Color greenDark =const Color(0xff00CBB3);
+Color greenLight = const Color(0xffEBF6F4);
+Color textColor = const Color(0xff20CBB3);
+Color backgroundColor = const Color(0xffF8F8F8);
